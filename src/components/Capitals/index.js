38d220c9788e4c7f6cbb86aa4f07771a6339b5b9
vijaykumardumpeti <vijaydumpeti70@gmail.com>
2022-11-22 09:26:45 +0000

    Capital App

---
+++ src/components/Capitals/index.js
@@ -0,0 +1,76 @@
+import {Component} from 'react'
+import './index.css'
+
+const countryAndCapitalsList = [
+  {
+    id: 'NEW_DELHI',
+    capitalDisplayText: 'New Delhi',
+    country: 'India',
+  },
+  {
+    id: 'LONDON',
+    capitalDisplayText: 'London',
+    country: 'United Kingdom',
+  },
+  {
+    id: 'PARIS',
+    capitalDisplayText: 'Paris',
+    country: 'France',
+  },
+  {
+    id: 'KATHMANDU',
+    capitalDisplayText: 'Kathmandu',
+    country: 'Nepal',
+  },
+  {
+    id: 'HELSINKI',
+    capitalDisplayText: 'Helsinki',
+    country: 'Finland',
+  },
+]
+
+// Write your code here
+export default class Capitals extends Component {
+  state = {
+    capital: countryAndCapitalsList[0].capitalDisplayText,
+  }
+
+  onUpdateState = event => {
+    this.setState({capital: event.target.value})
+  }
+
+  render() {
+    const {capital} = this.state
+
+    const resultObject = countryAndCapitalsList.find(
+      eachObject => eachObject.capitalDisplayText === capital,
+    )
+    console.log(resultObject)
+
+    return (
+      <>
+        <div className="bg-container">
+          <div className="white-container">
+            <h1>Countries And Capitals</h1>
+
+            <div className="input-para-container">
+              <select onChange={this.onUpdateState}>
+                {countryAndCapitalsList.map(eachObject => (
+                  <option
+                    key={eachObject.id}
+                    value={eachObject.capitalDisplayText}
+                  >
+                    {eachObject.capitalDisplayText}
+                  </option>
+                ))}
+              </select>
+
+              <p>is Capital of which country</p>
+            </div>
+            <p className="country-name">{resultObject.country}</p>
+          </div>
+        </div>
+      </>
+    )
+  }
+}
